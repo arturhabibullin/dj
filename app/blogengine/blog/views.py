bb7df48155403e17b4fa6dc2cd827580a39c9568
arturhabibullin <artur.habibullin@saria-volga.ru>
2021-09-14 13:04:25 +0300

    lesson 6 completed

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
 from .models import *
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
+from .utils import ObjectDetailMixin
 
+# Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request,'blog/index.html', context={'posts':posts})
+    return render(request, 'blog/index.html', context={'posts':posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post':post})
-
-def tags_list(request):
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+    
+def  tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags':tags})
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag':tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+   
